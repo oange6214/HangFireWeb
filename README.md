2e228968d9af6d634057fe4e5873d7061e4df57b
Pohan <oange6214@gmail.com>
2024-02-20 00:09:51 +0800

    First commit, Install Package and config setting.

---
+++ README.md
@@ -0,0 +1,3 @@
+# [WebAPI] .NET 8 - Background Jobs with Hangfire
+
+Hangfire 是一個開源的 .NET 库，用於在 .NET 應用程序中實現背景任務處理。它允許您以一種簡單而可擴展的方式處理後台任務，如發送電子郵件、生成報告、定期處理數據等。Hangfire 提供了一個可靠的方式來處理這些任務，並允許您監控它們的執行情況。
---
+++ src/Program.cs
@@ -0,0 +1,32 @@
+using Hangfire;
+using Hangfire.Storage.SQLite;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHangfire(config => config
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSQLiteStorage(builder.Configuration.GetConnectionString("Default")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHangfireDashboard();
+app.MapHangfireDashboard();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseHangfireDashboard();
+
+app.Run();
